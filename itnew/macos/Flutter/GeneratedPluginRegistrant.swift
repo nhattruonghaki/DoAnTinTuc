70b8089c93922ea6e423836de996a2e701aa1a32
Nhật Trường <144124468+nhattruonghaki@users.noreply.github.com>
2024-01-15 11:05:46 +0700

    Revert "Cao tri nhat"

--- itnew/macos/Flutter/GeneratedPluginRegistrant.swift
+++ itnew/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import path_provider_foundation
-import sqflite
-import url_launcher_macos
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
